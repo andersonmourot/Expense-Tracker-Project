1749e869be1e76c5a3331a832af789dc3403589c
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2022-11-30 17:50:44 -0600

    Added two helper functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,20 @@
 module.exports = {
-    format_date: (date) => {
-      return date.toLocaleDateString();
-    },
-  };
+  sortBySoonest: (Newexpense) => {
+    const currentDate = new Date();
+    return Newexpense.sort(function (a, b) {
+      let dateA = new Date(a.date);
+      let dateB = new Date(b.date);
+      // Move all the dates that are in the past to the end of the array
+      if (dateA < currentDate || dateB < currentDate) {
+        return -1;
+      }
+      // Order by soonest
+      return dateA - dateB;
+    });
+  },
+  sortByAmount: (Newexpense) => {
+    Newexpense.cost.sort(function(a, b) {
+      return a - b;
+    });
+  }
+};
